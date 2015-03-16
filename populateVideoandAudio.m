6f1f5b15269639f9d67c9bd31e09db71ed81e244
Shabbir Marzban <smarzban13@ku.edu.tr>
2015-03-16 17:57:10 +0200

    s
    s

--- populateVideoandAudio.m
+++ populateVideoandAudio.m
@@ -1,1 +1,33 @@
-function [audio, video]  = populateVideoandAudio(EmotionEvent,pathtoVideoFile)
+clc
+clear all
+close all
+
+load EmotionEvents
+
+EmotionEvent = EmotionEvents(1);
+
+pathtoVideoFile = 'C:\Users\Shabbir\Documents\GitHub\SCE_project\FrenchDatasetVideos\';
+
+
+
+%function [audio, video]  = populateVideoandAudio(EmotionEvent,pathtoVideoFile)
+videoFReader = vision.VideoFileReader([pathtoVideoFile,EmotionEvent.fileName,'.mp4']);
+videoPlayer = vision.VideoPlayer;
+
+startFrame = EmotionEvent.startTime/1000*30;
+endFrame = EmotionEvent.endTime/1000*30;
+
+
+for i = 1:startFrame-1
+[videoFrame] = step(videoFReader);
+end
+
+k = 1;
+while k < endFrame - startFrame
+  [videoFrame] = step(videoFReader);
+  step(videoPlayer, videoFrame);
+  k = k+1;
+end
+
+release(videoPlayer);
+release(videoFReader);
---
+++ test_AudioVideo.m
@@ -0,0 +1,7 @@
+clc
+clear all
+close all
+
+load EmotionEvents
+
+function [audio, video]  = populateVideoandAudio(EmotionEvent,pathtoVideoFile)
