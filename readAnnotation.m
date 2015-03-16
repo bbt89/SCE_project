6decb572274042fe094a911a27cb704d0eddbf0c
Shabbir Marzban <smarzban13@ku.edu.tr>
2015-03-16 17:00:49 +0200

    s
    s

--- readAnnotation.m
+++ readAnnotation.m
@@ -1,23 +1,24 @@
 clc
 clear all
+close all
+
 
 A = textscan(fileread('Annotation.txt'),'%s\t%f\t%f\t%s\t%s','Delimiter','\t');
 B  =cell(142,5);
 
 for i =1:length(A);
     if iscell(A{1,i})
-    B(:,i) =  A{1,i};
+        B(:,i) =  A{1,i};
     else
-    B(:,i) =  num2cell(A{1,i});
-
+        B(:,i) =  num2cell(A{1,i});
     end
 end
 
 EmotionEvents = cell2struct(B',{'label','startTime','endTime','description','fileName'});
 
 for i=1:length(EmotionEvents)
-    EmotionEvents(i).fileName = EmotionEvents(i).fileName(1:end-4); 
-
+    EmotionEvents(i).fileName = EmotionEvents(i).fileName(1:end-4);
+    
 end
 
 
