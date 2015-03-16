c672f9f91dfe46d26422133548b6847dfe420cc3
bbt89 <berkerturker@gmail.com>
2015-03-16 14:54:55 +0200

    b

---
+++ readAnnotation.m
@@ -0,0 +1,18 @@
+clc
+clear all
+
+A = textscan(fileread('Annotation.txt'),'%s\t%f\t%f\t%s\t%s','Delimiter','\t');
+B  =cell(142,5);
+
+for i =1:length(A);
+    if iscell(A{1,i})
+    B(:,i) =  A{1,i};
+    else
+    B(:,i) =  num2cell(A{1,i});
+
+    end
+end
+
+EmotionEvents = cell2struct(B',{'Label','startTime','endTime','Description','FileName'});
+
+
