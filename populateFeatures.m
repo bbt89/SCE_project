07e6ea28a1fdd38ce1b5ec97e529a890da9ff48f
bbt89 <berkerturker@gmail.com>
2015-03-16 16:42:03 +0200

    b

--- populateFeatures.m
+++ populateFeatures.m
@@ -1,24 +1,30 @@
-function [ feat ] = populateFeatures( EmotionEvents, extension)
+function [ feat ] = populateFeatures( EmotionEvents, flag)
 %populateFeatures Summary of this function goes here
-%   extension --> e.g. '_face', '_body' etc.
+%   flag --> 0: face, 1: body
 
-featTextPath='';
+featTextPath='textSource/';
 relativeTimetoMsecFactor=1/10000;
 
 sampleNum=length(EmotionEvents);
-files=unique(extractfield(EmotionEvents,'fileName'));
+allFiles=extractfield(EmotionEvents,'fileName');
+uniqueFiles=unique(allFiles);
+if(flag) 
+    nCols = 60; %body
+    extension='_body';
+else
+    nCols = 60; %face
+    extension='_face';
+end
 
+format = ['%d' repmat('%f', [1 nCols])];
 
-for i = 1:sampleNum
-    fidv = fopen(['../Session',Samples(i).fileName(5),'/dialog/MOCAP_rotated/',Samples(i).fileName,'.txt'],'r');
-    startFrame = round(Vfs*(Samples(i).startTime)/1000);
-    if startFrame < 1
-        startFrame = 1;
-    end
-    endFrame = round(Vfs*Samples(i).endTime/1000);
+for i = 1:length(uniqueFiles)
+    fidv = fopen([featTextPath,uniqueFiles(i),extension,'.txt'],'r');
+    data = textscan(fidv,format,'Delimiter','\n');
     
-    [~]=textscan(fidv,'%d %f %s',startFrame,'Delimiter','\n','Headerlines',2);
-    visseq(i).data=textscan(fidv,'%d %f %s',endFrame-startFrame,'Delimiter','\n');
+    find(allFiles,uniqueFiles(i));
+    
+    
     fclose(fidv);
     
     %disp(['done with ', num2str(i)]);
