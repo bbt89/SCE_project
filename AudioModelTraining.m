2f20575fe8ee0b24fcea444204dff1764ad355e7
bbt89 <berkerturker@gmail.com>
2015-04-06 15:02:03 +0300

    b

--- AudioModelTraining.m
+++ AudioModelTraining.m
@@ -6,8 +6,9 @@ REJECT = 3;
 
 load source/AudioSamplesMFCC_IEMOCAP.mat
 load source/AudioSamplesMFCC_AVLaughterCycle.mat
+load source/AudioSamplesMFCC_IEMOCAPreject.mat
 
-AffectDataSync=[AudioSamplesMFCC_IEMOCAP;AudioSamplesMFCC_AVLaughterCycle];
+AffectDataSync=[AudioSamplesMFCC_IEMOCAP;AudioSamplesMFCC_AVLaughterCycle;AudioSamplesMFCC_IEMOCAPreject];
 
 % label and feature extraction
 LABEL=extractfield(AffectDataSync,'label')';
@@ -26,6 +27,6 @@ end
 
 nfoldCV = 3;
 cRange=[-2 4 34];
-gRange=[-13 1 -8];
+gRange=[-13 1 -10];%gRange=[-13 1 -8];
 
 [model, bestParam, grid ]= learn_on_trainingData(data, label, cRange, gRange, nfoldCV, 0);
---
+++ AudioSamplesMFCCNormalization.m
@@ -0,0 +1,7 @@
+function [ outSamples ] = AudioSamplesMFCCNormalization( inSamples )
+%UNTÝTLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
